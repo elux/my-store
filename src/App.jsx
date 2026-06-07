0486fe427fb949d4c2382742d6b3eada4c23c1ac
elux <eluxdon@gmail.com>
2026-06-07 23:12:41 +0100

    fix basename for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <CartProvider>
-          <BrowserRouter>
+          <BrowserRouter basename="/my-store">
           <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
